d696e7d738e40a42173f1cb14f3cd3f164966da7
arudmessi4321 <arudmessi4321@gmail.com>
2022-08-08 15:05:06 +0530

    arudhra| set router

---
+++ src/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavBar } from "../shared/NavBar";
+import { Box } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import CustomStepper from '../shared/CustomStepper';
+
+
+export const Home = (props) => {
+
+
+  return (
+    <Box>
+      <NavBar user={props.user} navigate={props.navigate} />
+      <Box>
+        <CustomStepper navigate={props.navigate}/>
+      </Box>
+      <Routes>
+        <Route path="home/indiTable" element/>
+        <Route path="home/compTable" element/>
+      </Routes>
+    </Box>
+  );
+};
