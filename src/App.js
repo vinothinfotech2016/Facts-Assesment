54508ebb57f6bb8db0fe08cf949f162fb8298360
AnjaleeB <anjaleeb23@gmail.com>
2022-08-24 12:52:33 +0530

    anjalee|basic set for react-query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { AppNavigation } from "./components/navigation/Navigation";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
-      <AppNavigation />
+      <QueryClientProvider client={queryClient}>
+        <AppNavigation />
+      </QueryClientProvider>
     </div>
   );
 }
