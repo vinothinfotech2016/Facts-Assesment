a4f32b3365a898f0de773ccb8a4060b9824ceefc
sathish.tech <sathishkumarmanickam@vhitechlab.com>
2022-10-13 13:17:25 +0530

    sathish | added 10 facts

--- src/Main.js
+++ src/Main.js
@@ -8,39 +8,75 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export const Main = () => {
-  const [facts, setFacts] = useState("Not reloading");
-  const appApi = axios.create({
-    baseURL: "http://localhost:3000",
-  });
+  const [facts, setFacts] = useState([]);
+  const [factCount, setFactCount] = useState(10);
 
-  const fetchFacts = async () => {
-    axios.get("https://catfact.ninja/fact").then((response) => {
-      setFacts(response.data);
-    });
+  let allFacts = [];
+  const fetchFacts = async (newCount) => {
+    let iterationCount = newCount;
+    if (newCount === factCount) {
+      setFacts(allFacts);
+      return;
+    } else {
+      let newFact = await axios
+        .get("https://catfact.ninja/fact")
+        .then((response) => {
+          return response.data;
+        });
+
+      allFacts = allFacts.concat([newFact]);
+      fetchFacts((iterationCount += 1));
+    }
   };
   useEffect(() => {
-    fetchFacts();
+    fetchFacts(0);
   }, []);
 
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
         alignItems: "center",
         height: "100%",
       }}
     >
-      <Card sx={{ width: 500 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Fact Length -- {facts.length || "Loading Please Wait"}
-          </Typography>
-          <Typography variant="h5" component="div">
-            Fact --- {facts.fact || "Loading Please Wait"}
-          </Typography>
-        </CardContent>
-      </Card>
+      {facts.length === 10 ? (
+        facts.map((fact, factNo) => {
+          return (
+            <Card
+              sx={{
+                width: 300,
+                height: 300,
+                marginBottom: 10,
+                overflow: "scroll",
+              }}
+            >
+              <CardContent>
+                <Typography variant="h6" component="div" color={"blue"}>
+                  Fact -{factNo + 1}
+                </Typography>
+
+                <Typography variant="h5" component="div">
+                  Fact --- {fact.fact || "Loading Please Wait"}
+                </Typography>
+                <Typography
+                  sx={{ fontSize: 14, marginLeft: 10 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Fact Length -- {fact.length || "Loading Please Wait"}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <Typography variant="h5" component="div" sx={{ margin: 20 }}>
+          If You need some goods you should wait...
+        </Typography>
+      )}
     </div>
   );
 };
